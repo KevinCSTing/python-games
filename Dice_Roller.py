8a1b19fab2b357df483ba0e85d934571245fa318
Kevin Ting <kevsting@gmail.com>
2018-09-29 23:43:07 -0700

    Will now draw the die faces

--- Dice_Roller.py
+++ Dice_Roller.py
@@ -1,12 +1,43 @@
 import random
 
-def rollDice():
-    return random.randint(1,7)
+sides = "|       |"
+one =   "|   *   |"
+two =   "| *   * |"
+three = "| * * * |"
 
+
+def rollDice():
+    return random.randint(1,6)
+
+def drawDice(num):
+    s = "\n"
+    if(num == 1):
+        sec = (sides,one,sides)
+        print(s.join(sec))
+    elif(num == 2):
+        sec = (sides,two,sides)
+        print(s.join(sec))
+    elif(num == 3):
+        sec = (sides,three,sides)
+        print(s.join(sec))
+    elif(num == 4):
+        sec = (sides,two,two,sides)
+        print(s.join(sec))
+    elif(num == 5):
+        sec = (sides,two,one,two,sides)
+        print(s.join(sec))
+    elif(num == 6):
+        sec = (sides,three,three,sides)
+        print(s.join(sec))
+
+    
 def main(play):
     if(play.lower() == "y"):
-        print("You rolled number ",rollDice())
-        play_again = input("\nwould you like to play again? y/n ")
+        '''dice = rollDice()
+        drawDice(dice)'''
+        print("You rolled ")
+        drawDice(rollDice())
+        play_again = input("\nwould you like to play again? y/n \n")
         if(play_again.lower() == "y"):
             main(play_again)
         elif(play_again.lower() == "n"):
@@ -17,7 +48,7 @@ def main(play):
         print("okay. see ya!")
 
 if (__name__ == "__main__"):
-    play = input("Would you like to roll the dice? y/n")
+    play = input("Would you like to roll the dice? y/n \n")
     main(play)
 
 
